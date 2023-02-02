e77ddc72ddbddbb8d3cd6e46e2ecd54b58a9a6e0
Shibil-77 <shibi2772@gmail.com>
2023-02-02 21:49:01 +0530

    success

--- src/controllers/portController.ts
+++ src/controllers/portController.ts
@@ -1,23 +1,48 @@
 import { Express, Request, Response } from "express";
 import portSchema from "../models/portSchema"
 
-export const addChargingPort = async(req:Request,res:Response)=>{
-    try {
-    const {kW,rate,dayStart,dayEnd,timeStart,timeEnd,type,address,city,postalCode,country,userId} = req.body
-       if(req.body) {
-            console.log(dayEnd);
-            
-             const newPort  = new portSchema({
-                kW,rate,dayStart,dayEnd,timeStart,timeEnd,type,address,city,postalCode,country,userId,access:false
-             })
-              console.log(newPort);
-              
-             await newPort.save()
-              console.log(newPort);
-              
-             res.status(200).json({message:"success"})
-       }      
-    } catch (error) {
-            return res.status(500).json({message:"error"})
-    }
+export const addChargingPort = async (req: Request, res: Response) => {
+   try {
+      const { kW, rate, dayStart, dayEnd, timeStart, timeEnd, type, address, city, postalCode, country, userId } = req.body
+      if (req.body) {
+         const newPort = new portSchema({
+            kW, rate, dayStart, dayEnd, timeStart, timeEnd, type, address, city, postalCode, country, userId, access: false
+         })
+         await newPort.save()
+         res.status(200).json({ message: newPort.id })
+      }
+   } catch (error) {
+      return res.status(500).json({ message: "error" })
+   }
+}
+
+
+export const addMapValue = async (req: Request, res: Response) => {
+   try {
+      const { map, id } = req.body;
+      if (req.body) {
+         console.log(id);
+         const myArray = id.split("#");
+         console.log(myArray);
+         await portSchema.updateOne({ _id: myArray[1] }, { $set: { map: map } },)
+         res.status(200).json({ message: "MapValue successfully added" })
+      } else {
+         return res.status(404).json({ message: "server error" })
+      }
+   } catch (error) {
+      return res.status(404).json({ message: "server error" })
+   }
+}
+
+export const mapData = async (req: Request, res: Response) => {
+  try {
+   const mapValue = await portSchema.find()
+   console.log(mapValue);
+   
+   if(mapValue){
+      res.status(200).json(mapValue)
+   }
+  } catch (error) {
+   return res.status(500).json({ message: "server error" })
+  }
 }
--- src/routes/port.ts
+++ src/routes/port.ts
@@ -1,10 +1,14 @@
 import express from 'express'
 const authMiddleware = require("../middleware/authMiddleware");
-import {addChargingPort} from '../controllers/portController';
+import {addChargingPort,addMapValue,mapData} from '../controllers/portController';
 
 const router = express.Router()
 
 router.post('/addChargingPort',authMiddleware,addChargingPort)
+
+router.patch('/addMapValue',authMiddleware,addMapValue)
+
+router.get('/mapData',authMiddleware,mapData)
 
 
 export default router
