b204d1d46aa61d95541e5c752045f0ab70f3b275
Shibil-77 <shibi2772@gmail.com>
2023-03-03 20:08:47 +0530

    hosting

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ const app:Application =express()
 app.use(express.json())
 app.use(cors(
     {
-        origin: ["http://localhost:3000"],
+        origin: ["http://localhost:3000","https://smartshoping.club","https://www.smartshoping.club"],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         credentials: true,
         exposedHeaders: ["Content-Length", "X-Foo", "X-Bar"],
