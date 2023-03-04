ab3b4afa3bec0415727b68c130cb86ce0788bbd9
Shibil-77 <shibi2772@gmail.com>
2023-03-04 12:55:04 +0530

    htts

--- src/utils/authUtils.ts
+++ src/utils/authUtils.ts
@@ -18,7 +18,7 @@ export const emailSenders =async (email:string,id:string,fullName:string,url:str
         to: email,
         subject: "Email verification from powerZone",
         text:`Hi ${fullName},\n\nPlease verify your account by clicking 
-        http://smartshoping.club/${url}/${id}
+        https://smartshoping.club/${url}/${id}
         \nThanks `
       })
        setTimeout(async() => {
